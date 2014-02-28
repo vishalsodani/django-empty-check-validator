10db5e8b893a84e765162535f64e1ede81d48b47
Vishal Sodani <vishalsodani@rediffmail.com>
2014-02-28 09:49:24 +0530

    Add comment to show usage example

--- empty_check.py
+++ empty_check.py
@@ -1,5 +1,7 @@
 from django.core.exceptions import ValidationError
 
+# Usage example in a custom form
+# firstname = forms.CharField(validators = [EmptyCheck()])
 class EmptyCheck(object):
     def __call__(self, value):
         if len(value.strip()) == 0:
